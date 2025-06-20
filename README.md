fa8a65dd0d53652c911c68aac33ee1f7ffae29d4
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-20 15:10:52 -0300

    feat: Finishing server preset configs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rocketseat - API SOLID
+Nesse módulo da trilha Ignite da Rocketseat iremos trabalhar a criação de uma API utilizando os princípios SOLID em uma aplicação para gerenciamento de check-ins em uma academia.
+
+## Sobre
+
+## Tecnologias
+
+## Instalação
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import { app } from './app';
+import { app } from '@/app';
+import { env } from '@/env';
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333,
-}).then(
-  () => console.log(`server listening on port 3333...`)
-)
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => console.log(`server listening on port ${env.PORT}...`));
