ea3562a23e9b80db4c807414becf8c14125a49dc
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-25 17:22:16 -0300

    feat: Adding global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
 
 import { appRoutes } from '@/http/routes';
+import { env } from './env';
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO Send app error logs to an external source like Data Dog, ELK, NewRelic, Sentry.
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.'
+  })
+});
