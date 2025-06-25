d779970bdebcd447ff7af67b1a5357f0a79624eb
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-25 17:03:16 -0300

    feat: Adding first use case error to application

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 import { RegisterUseCase } from '@/use-cases/register';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -20,7 +21,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
     console.log(error);
-    return reply.status(409).send();
+
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    return reply.status(500).send();
   }
 
   return reply.status(201).send();
