36a40199f56980d35b500fa4c65dbb6b06904a19
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-02 11:19:32 -0300

    feat: creating gyms repository and in-memory gyms database

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,16 @@
+import { GymsRepository } from '@/repositories/gyms-repository';
+import { Gym } from '@prisma/client';
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = [];
+
+  async findById(id: string): Promise<Gym | null> {
+    const gym = this.items.find((gym) => gym.id === id);
+
+    if (!gym) {
+      return null;
+    }
+
+    return gym;
+  }
+}
