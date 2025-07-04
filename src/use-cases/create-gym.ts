fd24a7c3ad7a3cc9c6a5d7c4494f61bdaaf370d6
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-04 00:30:28 -0300

    feat: Creating create gym use case

---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,33 @@
+import { GymsRepository } from '@/repositories/gyms-repository';
+
+interface CreateGymUseCaseRequest {
+  title: string;
+  description: string | null;
+  phone: string | null;
+  latitude: number;
+  longitude: number;
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: CreateGymUseCaseRequest) {
+    const gym = await this.gymsRepository.create({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude,
+    });
+
+    return {
+      gym,
+    };
+  }
+}
