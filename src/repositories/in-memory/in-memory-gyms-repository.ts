e8eaaa1343f162d899537a9f76d7b0781c81c6b9
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-04 00:32:04 -0300

    feat: Implementing create method to repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,27 @@
+import { Gym, Prisma } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
+import { randomUUID } from 'node:crypto';
+
 import { GymsRepository } from '@/repositories/gyms-repository';
-import { Gym } from '@prisma/client';
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+
+    this.items.push(gym);
+
+    return gym;
+  }
 
   async findById(id: string): Promise<Gym | null> {
     const gym = this.items.find((gym) => gym.id === id);
