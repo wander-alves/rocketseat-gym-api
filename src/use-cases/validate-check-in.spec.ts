eeef0c05a8e68dc0c06f31760acb5fa2f8ba9c34
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-10 00:37:52 -0300

    feat: adding rule to only permit check-in validation at max after 20 minutes of its creation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,8 +1,9 @@
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { ValidateCheckInUseCase } from './validate-check-in';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -12,11 +13,11 @@ describe('Validate Check-in Use Case', () => {
     inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(inMemoryCheckInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it('should be able to validate the check-in', async () => {
@@ -41,5 +42,23 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       });
     }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2025, 0, 1, 10, 40));
+    const createdCheckIn = await inMemoryCheckInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    });
+
+    const twentyMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyMinutesInMs);
+
+    await expect(async () => {
+      await sut.execute({
+        checkInId: createdCheckIn.id,
+      });
+    }).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
