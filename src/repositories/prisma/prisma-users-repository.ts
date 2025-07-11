1d03d61165dcbc682376e41378ad714ae734e0ea
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-11 01:04:43 -0300

    feat: implementing Prisma Users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
