0cf2cef9f33862754cc49aae46d5e1e6a9b68fe8
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-11 01:03:20 -0300

    fix: renaming findById parameter

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -5,7 +5,7 @@ export interface CheckInsRepository {
     userId: string,
     checkInDate: Date,
   ): Promise<CheckIn | null>;
-  findById(checkInId: string): Promise<CheckIn | null>;
+  findById(id: string): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   countByUserId(userId: string): Promise<number>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
