4552270a4c5c56f43bbb2b07c29f690ace57f923
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-19 13:12:39 -0300

    feat: declaring new routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { register } from '@/http/controllers/register';
-import { authenticate } from '@/http/controllers/autheticate';
+import { authenticate } from '@/http/controllers/authenticate';
+import { profile } from './controllers/profile';
 
 export async function appRoutes(app: FastifyInstance) {
+  // Public
   app.post('/users', register);
-
   app.post('/sessions', authenticate);
+
+  // Private
+  app.get('/me', profile);
 }
