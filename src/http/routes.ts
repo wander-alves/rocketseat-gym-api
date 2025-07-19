f1ea0c65ac77344c2378b78e7b5c54af52124622
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-19 14:06:03 -0300

    feat: implementing auth middleware to private routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,12 +2,11 @@ import { FastifyInstance } from 'fastify';
 import { register } from '@/http/controllers/register';
 import { authenticate } from '@/http/controllers/authenticate';
 import { profile } from './controllers/profile';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
-  // Public
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
-  // Private
-  app.get('/me', profile);
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
