4fc3c7645b011fa10596bc9f42ac14bf59b91290
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-21 21:58:53 -0300

    feat: installing e2e dependencies and creating register controller test

---
+++ src/http/controllers/register.spec.ts
@@ -0,0 +1,19 @@
+import { app } from '@/app.ts';
+import request from 'supertest';
+import { describe, it, expect, beforeAll } from 'vitest';
+
+describe('Register (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  it('should be able to register an account', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'strongpassword',
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
