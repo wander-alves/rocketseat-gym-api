c6257a05dfbb184fe9de1edc51f42dee8c2420d3
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-23 00:45:40 -0300

    feat: adding E2E get profile tests

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,33 @@
+import { app } from '@/app.ts';
+import request from 'supertest';
+import { describe, it, expect, beforeAll } from 'vitest';
+
+describe('Profile (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  it('should be able to get account profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'strongpassword',
+    });
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: 'strongpassword',
+    });
+    const { token } = authResponse.body;
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.user).toEqual({
+      email: 'john.doe@example.com',
+    });
+  });
+});
