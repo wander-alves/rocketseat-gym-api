9823fd1edd8d3e6fe87c116fb80430c48d065142
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-07-29 23:52:23 -0300

    feat: creating create user utilitary

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from 'fastify';
+import request from 'supertest';
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await request(app.server).post('/users').send({
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    password: 'strongpassword',
+  });
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    email: 'john.doe@example.com',
+    password: 'strongpassword',
+  });
+
+  const { token } = authResponse.body;
+
+  return {
+    token,
+  };
+}
