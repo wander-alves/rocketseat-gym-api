adb565b9145ff045c3cb7c4a91338962ab0770a8
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-08-01 00:41:30 -0300

    feat: implementing refresh token strategy

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,35 @@
+import { app } from '@/app.ts';
+import request from 'supertest';
+import { describe, it, expect, beforeAll } from 'vitest';
+
+describe('Refresh Token(E2E)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'strongpassword',
+    });
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: 'strongpassword',
+    });
+
+    const cookies = authResponse.get('Set-Cookie');
+
+    const response = await request(app.server)
+      .patch('/refresh')
+      .set('Cookie', cookies!)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+    expect(response.body).toEqual([expect.stringContaining('refreshToken=')]);
+  });
+});
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { register } from '@/http/controllers/users/register';
 import { authenticate } from '@/http/controllers/users/authenticate';
 import { profile } from './profile';
 import { verifyJWT } from '../../middlewares/verify-jwt';
+import { refresh } from '@/http/controllers/users/refresh.ts';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+  app.patch('/refresh', refresh);
 
   app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
