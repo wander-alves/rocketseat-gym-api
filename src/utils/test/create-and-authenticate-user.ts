7d53f067754e22085e1966cf03ab2fe06711f21e
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-08-02 11:27:05 -0300

    test: updating account creation utils to add user role

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { prisma } from '@/lib/prisma.ts';
+import { hash } from 'bcryptjs';
 import { FastifyInstance } from 'fastify';
 import request from 'supertest';
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    password: 'strongpassword',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hash('strongpassword', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   });
 
   const authResponse = await request(app.server).post('/sessions').send({
